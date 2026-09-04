898ddba66bfc3e6423adf21fc41d8c7c3e710f7c
qwlsx <ruslan.rebo@hotmail.com>
2026-09-04 09:29:56 +0300

    testing

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
-
-echo "Hello, PHP!";
+$test = 10;
+$test = 3.1;
+$test = "Hello";
+$test = 'õpilane';
+$test = '🎲';
+$test = true;
+$test = false;
+$test = [1, true, 'random'];
+$test = new stdClass();
+$test->name = 'Ruslan';
+var_dump($test);
